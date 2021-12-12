052a2f8c8db05363e01526176825149524a4afec
Marco Ávila <avila.marco.dev@gmail.com>
2021-12-12 17:47:32 +0000

    add projects

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,44 @@ import Projects from "./Components/Projects";
 import image1 from "./media/images/rosa-fardas.png";
 import image2 from "./media/images/rodolfo-costa-website.png";
 import image3 from "./media/images/firegram.png";
+import image4 from "./media/images/housingprocess.png";
+import image5 from "./media/images/iguarias.png";
 
 const App: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   const rosaFardas: string = image1;
   const rodolfoCosta: string = image2;
   const fireGram: string = image3;
+  const bmf: string = image4;
+  const iguarias: string = image5;
   const projects = [
     {
       id: 1,
+      name: "Iguarias da Vila",
+      subName: "Restaurant",
+      description1:
+        "Created a landing page for a local restaurant where i used React (Typescript), EmailJs and Material-Ui.",
+      description2: "Created the design and implemented SEO.",
+      link1: "https://iguarias-514d2.web.app/",
+      link2: "https://github.com/Markkex/iguarias",
+      image: iguarias,
+      option1: "Landing Page",
+      option2: "Check it on Github",
+    },
+    {
+      id: 2,
+      name: "BMF",
+      subName: "Housing App",
+      description1:
+        "Application where allows the user to create a new process related to a house and permits counting the number of days the process is active. After the process is finished you can download a document with all the finished processes.",
+      description2:
+        "Used React Router, React (Typescript), Material-UI and xml library to create documents.",
+      link1: "https://github.com/Markkex/BMF",
+      option1: "Check it on Github",
+      image: bmf,
+    },
+    {
+      id: 3,
       name: "This website",
       subName: "",
       description1:
@@ -25,19 +54,18 @@ const App: FC = () => {
       option1: "Check it on Github",
     },
     {
-      id: 2,
+      id: 4,
       name: "Firegram",
       subName: "Instagram like Social App",
-      description1:
-        "User can insert pictures and see the uploaded ones.",
+      description1: "User can insert pictures and see the uploaded ones.",
       link1: "https://firegram-b985e.web.app/",
       link2: "https://github.com/Markkex/Firegram",
       image: fireGram,
       option1: "Check the page",
-      option2: "Check it on Github"
+      option2: "Check it on Github",
     },
     {
-      id: 3,
+      id: 5,
       name: "Rodolfo Costa",
       subName: "Tattoo and Sculpture",
       description1:
@@ -49,7 +77,7 @@ const App: FC = () => {
       option1: "Landing Page",
     },
     {
-      id: 4,
+      id: 6,
       name: "Rosa Fardas",
       subName: "Comércio de Vestuário, Lda",
       description1:
@@ -60,19 +88,19 @@ const App: FC = () => {
       image: rosaFardas,
       option1: "Landing Page",
       option2: "E-Commerce",
-    },  
+    },
   ];
   return (
-    <div className='App'>
-      <div className='bg'></div>
+    <div className="App">
+      <div className="bg"></div>
 
       {open && <MobileMenu setOpen={setOpen} />}
       {open === false && (
         <Fragment>
           <Menu setOpen={setOpen} />
           <Main />
-          <div id='projects' className='projects'>
-            <h1 className='header-font-style'>Projects</h1>
+          <div id="projects" className="projects">
+            <h1 className="header-font-style">Projects</h1>
             {projects.map((project) => (
               <Projects data={project} />
             ))}
